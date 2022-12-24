62746ed72c3329da9d0ea7ac0a883a5c5735a108
Sreehari <95231762+vr-sreehari@users.noreply.github.com>
2022-12-24 13:39:01 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
   loader(msgDiv);
 
   //fetch data from server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://srees-bot.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
